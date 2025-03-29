7409e9b80df60edf03055914bd6fa493615c4dca
okumura chie <neco9145@me.com>
2025-03-29 10:51:46 +0900

    3/24-3/29

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 class AuthController extends Controller
 {
     public function index()
     {
-        return view('index')->with('message','ログインしました');;
+        return view('index');
     }
+
 }
+    /*
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->with('message','ログインしました'); // ログイン成功時
+        }
+
+        return back()->withErrors([
+            'email' => 'メールアドレスまたはパスワードが正しくありません。',
+        ]);
+    }
+*/
