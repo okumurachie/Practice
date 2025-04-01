95d121d74ae6262a25fd2b03fe5051e39923a006
okumura chie <neco9145@me.com>
2025-04-01 15:22:25 +0900

    出品ページ・商品リンク修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,7 +15,7 @@
     <div class="register__alert--danger">
         <ul>
             @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
+            <li>{{$error}}</li>
             @endforeach
         </ul>
     </div>
@@ -27,39 +27,38 @@
             <h2>ファッション・小物類</h2>
         </div>
         <div class="lineup">
-                <h3>おすすめラインナップ</h3>
+            <h3>おすすめラインナップ</h3>
         </div>
         <div class="content__box">
             @foreach($products as $product)
-            <div class="content__box-item">
-                <p>
-                    <a href="/details/{{$product['id']}}" class="content__id">{{$product['id']}}</a>
-                </p>
-                <div class="content__inner">
-                    <div class="content__img">
-                        <img src="{{asset($product['image'])}}" alt="{{$product['name']}}">
-                    </div>
-                    <div class="content__detail-1">
-                        <div class="content__name">
-                            <p>{{$product['name']}}</p>
-                            @if(isset($product->purchases->created_at))
-                            <p>{{$product->purchases->created_at}}</p>
-                            @endif
+            <a href="/details/{{$product['id']}}" class="content__id">
+                <div class="content__box-item">
+                    <div class="content__inner">
+                        <div class="content__img">
+                            <img src="{{asset($product['image'])}}" alt="{{$product['name']}}">
                         </div>
-                        <div class="content__price">
-                            <p>{{"¥".number_format($product['price'])}}</p>
+                        <div class="content__detail-1">
+                            <div class="content__name">
+                                <p>{{$product['name']}}</p>
+                                @if(isset($product->purchases->created_at))
+                                <p>{{$product->purchases->created_at}}</p>
+                                @endif
+                            </div>
+                            <div class="content__price">
+                                <p>{{"¥".number_format($product['price'])}}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="content__detail-2">
-                        <p>{{$product['comment']}}</p>
+                        <div class="content__detail-2">
+                            <p>{{$product['comment']}}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
             @endforeach
         </div>
     </div>
 </div>
 <div class="pagination">
-{{$products->links()}}
+    {{$products->links()}}
 </div>
 @endsection
--- src/resources/views/product.blade.php
+++ src/resources/views/product.blade.php
@@ -15,72 +15,72 @@
     <div class="register__alert--danger">
         <ul>
             @foreach($errors->all() as $error)
-                <li>{{$error}}</li>
+            <li>{{$error}}</li>
             @endforeach
         </ul>
-    @endif
-</div>
-
-<div class="register__content">
-    <div class="register-form__heading">
-        <h2>出品登録</h2>
+        @endif
     </div>
-    <form class="form" action="/products" method="post" enctype="multipart/form-data">
-        @csrf
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">出品名</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="text" name="name" value="{{ old('name') }}" />
-                </div>
-                <div class="form__error">
-                    @error('name')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
+
+    <div class="register__content">
+        <div class="register-form__heading">
+            <h2>出品登録</h2>
         </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">価格</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="number" name="price" value="{{ old('price') }}">
+        <form class="product-form" action="/products" method="post" enctype="multipart/form-data">
+            @csrf
+            <div class="product-form__group">
+                <div class="product-form__group-title">
+                    <span class="product-form__label--item">出品名</span>
                 </div>
-                <div class="form__error">
-                    @error('price')
-                    {{ $message }}
-                    @enderror
-                </div>
-            </div>
-        </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">商品画像</span>
-            </div>
-            <div class="form__group-content">
-                <input type="file" id="imageInput" accept="image/*" name="image">
-                    <div class="form__img">
-                        <img id="preview"  alt="image preview" style="max-width: 200px; display: none;">
+                <div class="product-form__group-content">
+                    <div class="product-form__input--text">
+                        <input type="text" name="name" value="{{ old('name') }}" />
+                    </div>
+                    <div class="product-form__error">
+                        @error('name')
+                        {{ $message }}
+                        @enderror
                     </div>
                 </div>
-        </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">商品説明</span>
             </div>
-            <div class="form__group-content">
-                <div class="form__input--textarea">
-                    <textarea name="comment" cols="60" rows="5">{{old('comment','')}}</textarea>
+            <div class="product-form__group">
+                <div class="product-form__group-title">
+                    <span class="product-form__label--item">価格</span>
+                </div>
+                <div class="product-form__group-content">
+                    <div class="product-form__input--text">
+                        <input type="number" name="price" value="{{ old('price') }}">
+                    </div>
+                    <div class="product-form__error">
+                        @error('price')
+                        {{ $message }}
+                        @enderror
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="form__button">
-            <button class="form__button-submit" type="submit">登録</button>
-        </div>
-    </form>
-</div>
-@endsection
+            <div class="product-form__group">
+                <div class="product-form__group-title">
+                    <span class="product-form__label--item">商品画像</span>
+                </div>
+                <div class="product-form__group-content">
+                    <input type="file" id="imageInput" accept="image/*" name="image">
+                    <div class="product-form__img">
+                        <img id="preview" alt="image preview" style="max-width: 200px; display: none;">
+                    </div>
+                </div>
+            </div>
+            <div class="product-form__group">
+                <div class="product-form__group-title">
+                    <span class="product-form__label--item">商品説明</span>
+                </div>
+                <div class="product-form__group-content">
+                    <div class="product-form__input--textarea">
+                        <textarea name="comment" cols="60" rows="5">{{old('comment','')}}</textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="product-form__button">
+                <button class="product-form__button-submit" type="submit">登録</button>
+            </div>
+        </form>
+    </div>
+    @endsection
